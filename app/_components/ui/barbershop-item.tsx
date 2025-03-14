14ebaad1d84dc794722136b48aa95ccaac6eccc4
Lindinei40 <saleslindinei0@gmail.com>
2025-03-13 21:38:07 -0300

    correção rápida

---
+++ app/_components/ui/barbershop-item.tsx
@@ -0,0 +1,11 @@
+import { Barbershop } from "@prisma/client"
+
+interface BarbershopItemProps {
+  barbershop: Barbershop
+}
+
+const BarbershopItem = ({}: BarbershopItemProps) => {
+  return <h1></h1>
+}
+
+export default BarbershopItem
---
+++ app/_providers/auth.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { SessionProvider } from "next-auth/react"
+import { ReactNode } from "react"
+
+const AutProvider = ({ children }: { children: ReactNode }) => {
+  return <SessionProvider>{children}</SessionProvider>
+}
+
+export default AutProvider
